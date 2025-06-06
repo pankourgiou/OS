916dc65d285c215ef937747f514ed88767f31238
L <60981745+pankourgiou@users.noreply.github.com>
2025-06-06 20:13:32 +0300

    Update OS.py

--- OS.py
+++ OS.py
@@ -1,5 +1,5 @@
 #I will repeat those ideas so that I Operate on a dogville "tempo" life style and philosophy
-#print("Dogville is my OS, Planet X band is nice")
+#print("I'll keep my Windows OS plus Kodachi Linux, Planet X band is nice")
 #Problem is they lie and lies go cloud!!! if I were a philosopher I wouldn't be able to make a bread? if not bread would be fake!!!simple!Say the truth to friends colleagues everyone! The truth is out there..
 #If CIA document proves true which is most possibly true this means mass media lie and hide truth many years now you know...
 #A simple comments file
